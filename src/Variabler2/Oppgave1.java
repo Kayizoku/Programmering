0a29c81050b3908b4e1654fab9907df7bce51f73
MTK <tayyabkhalid306@gmail.com>
2018-09-18 20:03:24 +0200

    Oppgave 1 in Variables2 done

---
+++ src/Variabler2/Oppgave1.java
@@ -0,0 +1,19 @@
+package Variabler2;
+
+import static javax.swing.JOptionPane.*;
+
+public class Oppgave1 {
+
+    public static void main(String[] args) {
+
+       String innAlder = showInputDialog("Skriv inn din alder:");
+       int alder = Integer.parseInt(innAlder);
+    showMessageDialog(null, "Min alder er altså "+alder+ " år gammel");
+
+
+
+
+
+}
+
+}
